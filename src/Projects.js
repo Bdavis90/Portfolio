eb9c7128283ade473abaa8a44f55958c0c9534b2
Brandon Davis <wowzerss90@yahoo.com>
2024-03-26 12:21:33 -0400

    updated description

--- src/Projects.js
+++ src/Projects.js
@@ -10,14 +10,14 @@ import RB4 from "../images/RealmBladeBuffs.png"
 export const PROJECTS = [
     {
         title: "Realm Blade",
-        description: "Realm Blade is a 3D rogue lite that takes gameplay inspiration from games like Soul Knight and Binding of Issac but with a 3D aesthetic. The main gameplay loop consists of the player progressing through levels wherein they need to combat a round of enemies in order to progress to the next level.",
+        description: "Realm Blade is a 3D rogue lite that takes gameplay inspiration from games like Soul Knight and Binding of Issac but with a 3D aesthetic. The main gameplay loop consists of the player progressing through levels wherein they need to combat a round of enemies in order to progress to the next level. Created in 4 months with a team of 5 developers.",
         images: [RB0, RB1, RB2, RB3, RB4],
         link: "https://bigpapaergodzillas.itch.io/realm-blade",
         source: ""
     },
     {
         title: "A Hard Pill To Swallow",
-        description: "A Hard Pill To Swallow is a wave based shooter game created in a month with a team of 5 developers.",
+        description: "A Hard Pill To Swallow is a first-person wave based shooter where you try to survive as long as you can. Created in a month with a team of 5 developers.",
         images: [HPTS0, HPTS1],
         link: "https://simmer.io/@BigPaperGodzla/a-hard-pill-to-swallow",
         source: "https://github.com/Bdavis90/FantasticStarsPPII"
