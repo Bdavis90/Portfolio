00a18dadea121a3d3b27dddf5be0c6263cb34030
Brandon Davis <wowzerss90@yahoo.com>
2024-04-04 16:06:39 -0400

    removed source link

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,7 +6,7 @@ export default function Project({ title, description, images, link, source }) {
         </div>
         <p className="description">{description}</p>
         {link && <a className="button" href={link}>Project</a>}
-        {source && <a className="button" href={source}>Code</a>}
+        {/* {source && <a className="button" href={source}>Code</a>} */}
         <hr />
     </>
 }
