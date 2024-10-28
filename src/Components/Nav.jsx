14203b02eb92aad5dfa48b7c91ea86506bef62c9
Brandon Davis <wowzerss90@yahoo.com>
2024-10-28 07:27:08 -0400

    removed nav link

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,14 @@
-
-
 export default function Nav() {
-    return <menu>
-        <ul>
-            <li><a href="#header">Home</a></li>
-            <li><a href="#portfolio">Portfolio</a></li>
-            <li><a href="Brandon Davis Gameplay Programmer Resume.pdf">Resume</a></li>
-        </ul>
+  return (
+    <menu>
+      <ul>
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="Brandon Davis Gameplay Programmer Resume.pdf">Resume</a>
+        </li>
+      </ul>
     </menu>
+  );
 }
